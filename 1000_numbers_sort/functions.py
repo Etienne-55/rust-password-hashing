86ea8f44e78bf5a02317dfe186e2e20e699af758
etienne <etienne.pautet@gmail.com>
2024-08-22 15:38:08 -0300

    first commit

---
+++ 1000_numbers_sort/functions.py
@@ -0,0 +1,48 @@
+import random
+
+numbers = []
+
+def random_numbers():
+    
+
+    for _ in range(1000):
+        numbers.append(random.randint(0,1000))
+    
+
+random_numbers()
+print(numbers)
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    
+    return merge(left, right)
+
+def merge(left, right):
+    merged = []
+    i = j = 0
+    
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+    
+    while i < len(left):
+        merged.append(left[i])
+        i += 1
+    
+    while j < len(right):
+        merged.append(right[j])
+        j += 1
+    
+    return merged
+
+sorted_numbers = merge_sort(numbers)
+print(sorted_numbers)
---
+++ 1000_numbers_sort/main.py
@@ -0,0 +1,13 @@
+from functions import random_numbers, merge_sort
+
+
+#1 print numbers
+#show insorted 
+#show sorted
+#delete
+#exit
+
+
+random_numbers()
+
+merge_sort()
