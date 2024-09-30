e5c3a2eb2d8cd5a3cf196feb4acc2217d7e8b23d
etienne <etienne.pautet@gmail.com>
2024-09-30 09:39:10 -0300

    rust files

---
+++ rust-teste/projetotest/src/bubble_sort.rs
@@ -0,0 +1,15 @@
+pub fn bubble_sort_func(arr: &mut [i32]) {
+    let mut n = arr.len();
+    let mut swapped = true;
+
+    while swapped {
+        swapped = false;
+        for i in 1..n {
+            if arr[i - 1] > arr[i] {
+                arr.swap(i - 1, i);
+                swapped = true;
+            }
+        }
+        n -= 1;
+    }
+}
