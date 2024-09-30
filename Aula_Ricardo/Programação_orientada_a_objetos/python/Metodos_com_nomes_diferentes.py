42d8dac07cfd7d2bb4905693ff2ddb0fe144263b
etienne <etienne.pautet@gmail.com>
2024-09-30 11:20:08 -0300

    file removal

--- Aula_Ricardo/Programação_orientada_a_objetos/python/Metodos_com_nomes_diferentes.py
+++
@@ -1,9 +0,0 @@
-class Calculadora: 
-    def somar(self,a, b, c= None):
-        if c is not None:
-            return a + b + c
-        return a + b
-
-calc = Calculadora()
-print(calc.somar(1, 2))
-print(calc.somar(1, 2, 3))
--- Aula_Ricardo/Programação_orientada_a_objetos/python/Métodos_estáticos.py
+++
@@ -1,9 +0,0 @@
-class Matematica:
-    @staticmethod
-    def fatorial(n):
-        if n == 0:
-            return 1
-        else:
-            return n * Matematica.fatorial(n-1)
-        
-print(Matematica.fatorial(5))
